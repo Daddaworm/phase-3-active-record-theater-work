ec414b1598cf63efa9dafb44e93517f69f1f077e
Ethan Rodriguez <ethan.rodriguez@mac.com>
2021-07-25 22:50:56 -0400

    Completed assignment

--- app/models/role.rb
+++ app/models/role.rb
@@ -4,7 +4,6 @@ class Role < ActiveRecord::Base
 
     def actors 
         # returns an array of names from the actors associated with this role
-
         self.auditions.pluck(:actor)
     end
 
@@ -17,9 +16,22 @@ class Role < ActiveRecord::Base
 
     def lead
         # returns the first instance of the audition that was hired for this role or returns a string 'no actor has been hired for this role'
-        self.auditions.find_by("hired == ?", true)
-        puts "no actor has been hired for this role"
+        if (self.auditions.find_by("hired == ?", true))
+            self.auditions.find_by("hired == ?", true)
+        else
+            puts 'no actor has been hired for this role'
+        end
     end
-    
+
+
+    def understudy
+        # returns the second instance of the audition that was hired for this role or returns a string 'no actor has been hired for understudy for this role'
+        if (self.auditions.where("hired = ?", true))
+            (self.auditions.where("hired = ?", true)).second
+        else
+            puts 'no actor has been hired for this role'
+        end
+    end
+
 
 end
